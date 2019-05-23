e92ecb3d1628b577c5bde2dce6dafc2f3a0a36e5
Matt Wilkin <mwilkin22@gmail.com>
2019-05-23 16:39:35 -0700

    part 2 working, reading pair-programming.txt."

--- index.js
+++ index.js
@@ -3,9 +3,29 @@
 const fs = require('fs');
 
 let names = ['Tia, Flowey, Maggie'];
+
 let longString = [`'use strict';\n\nlet names=['Flowy', 'Tia', 'Sparky'];\n\nlet stringifyBuffer = () =>{\n  let str = '';\n  for( let char of buffer){\n    str += String.fromCharCode(char);\n  }\n  return str;\n};\n`];
 
 let data = Buffer.from('');
+
+let fileCreator = (source) => {
+  fs.writeFile('./files/loop.js', source, 'utf8', (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+    names.forEach(element => {
+      console.log(element.split(','));
+    });
+  });
+};
+
+let fileCreation = (array) => {
+  array.forEach(element => {
+    data = Buffer.concat([data, Buffer.from(element)]);
+  });
+  fileCreator(data);
+};
+
+fileCreation(longString);
 
 let stringifyBuffer = (data) => {
   let str = '';
@@ -15,22 +35,12 @@ let stringifyBuffer = (data) => {
   return str;
 };
 
-let fileWriter = (source) => {
-  fs.writeFile('./files/loop.js', source, 'utf8', (err) => {
+let fileReader = () => {
+  fs.readFile('./files/pair-programming.txt',(err, data) => {
     if (err) throw err;
-    console.log('The file has been saved!');
-    names.forEach(element => {
-      console.log(element.split(','));
-    });
+    console.log(data);
+    console.log(stringifyBuffer(data));
   });
 };
-let fileCreation = (array) => {
-  array.forEach(element => {
-    data = Buffer.concat([data, Buffer.from(element)]);
-  });
-  fileWriter(data);
-};
 
-fileCreation(longString);
-
-
+fileReader();

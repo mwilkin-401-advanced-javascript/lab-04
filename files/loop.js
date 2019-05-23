8e73d767c4a0bf538754047f1c907f2f8d924c59
Matt Wilkin <mwilkin22@gmail.com>
2019-05-23 15:53:59 -0700

    part 1 complete?

---
+++ files/loop.js
@@ -0,0 +1,11 @@
+'use strict';
+
+ 'Flowy' 'Tia' 'Sparky'
+
+ let stringifyBuffer = () =>{
+ let str = '';
+ for( let char of buffer){
+ str += String.fromCharCode(char);
+ }
+ return str;
+};
--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 'use strict';
+
 const fs = require('fs');
 
-// let buffer = Buffer.from('Wow, this topic is really cool!');
-// console.log(buffer);
-// console.log(buffer.toString());
+let names = ['Tia, Flowy, Maggie'];
+let longString = [`'use strict';\n\n 'Flowy' 'Tia' 'Sparky'\n\n let stringifyBuffer = () =>{\n let str = '';\n for( let char of buffer){\n str += String.fromCharCode(char);\n }\n return str;\n};\n`];
 
-// let stringifyBuffer = () =>{
-//   let str = '';
-//   for( let char of buffer){
-//   str += String.fromCharCode(char);  
-//   }
-// return str;
-// };
+let data = Buffer.from('');
 
+let stringifyBuffer = (data) => {
+  let str = '';
+  for(let character of data){
+    str += String.fromCharCode(character);
+  }
+  return str;
+};
 
+let fileWriter = (source) => {
+  fs.writeFile('./files/loop.js', source, 'utf8', (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+    names.forEach(element => {
+      console.log(element);
+    });
+  });
+};
+let fileCreation = (array) => {
+  array.forEach(element => {
+    data = Buffer.concat([data, Buffer.from(element)]);
+  });
+  fileWriter(data);
+};
 
-fs.writeFile('./files/loop.js', 'Hello Node.js', 'utf8', (err) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
-});
-
-
+fileCreation(longString);

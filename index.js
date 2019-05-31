4a3afe9ae553253ff41d7bccc568382e789357d3
Matt Wilkin <mwilkin22@gmail.com>
2019-05-30 22:41:21 -0700

    begin refactor following in-class demo.

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 'use strict';
 
 const fs = require('fs');
+const reader = require('readline');
 
 let names = ['Tia, Flowey, Maggie'];
 
 let longString = [`'use strict';\n\nlet names=['Flowy', 'Tia', 'Sparky'];\n\nlet stringifyBuffer = () =>{\n  let str = '';\n  for( let char of buffer){\n    str += String.fromCharCode(char);\n  }\n  return str;\n};\n`];
 
-let data = Buffer.from('');
-
 let fileCreator = (source) => {
   fs.writeFile('./files/loop.js', source, 'utf8', (err) => {
     if (err) throw err;
-    console.log('The file has been saved!');
-    names.forEach(element => {
-      console.log(element.split(','));
-    });
+    // console.log('The file has been saved!');
+    // names.forEach(element => {
+    //   console.log(element.split(','));
   });
 };
+
+let data = Buffer.from('');
 
 let fileCreation = (array) => {
   array.forEach(element => {
@@ -46,3 +46,6 @@ let fileReader = () => {
 
 
 fileReader();
+
+
+module.exports = fileCreation, fileWriter;
